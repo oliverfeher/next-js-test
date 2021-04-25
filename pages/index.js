c1d953b4574637139efc9042d3d2056285179054
Oliver Feher <olcsee@gmail.com>
2021-04-25 19:55:37 -0400

    fixing it."

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
-import Head from 'next/head'
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
+
 import Layout from '../components/layout'
-import { getAllPostsForHome } from '../docs/lib/api'
-import { CMS_NAME } from '../docs/lib/constants'
-import { getAllPostsWithSlug, getPostAndMorePosts } from '../docs/lib/api'
+import { getAllPostsWithSlug} from '../docs/lib/api'
 import Deal from '../components/deal'
 
 export default function Index({ allDeals: { edges }, preview }) {
-  const heroPost = edges[0]?.node
-  const morePosts = edges.slice(1)
 
   console.log(edges)
   return (
